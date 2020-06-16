a57e48ce25ce7a6d2e7e9c1b645b6ddecf21463f
jhelm3 <jakehelm36@gmail.com>
2020-06-16 16:01:24 -0600

    department and random password

---
+++ src/email/Email.java
@@ -0,0 +1,60 @@
+package email;
+
+import java.util.Scanner;
+
+public class Email {
+  private String firstName;
+  private String lastName;
+  private String password;
+  private String department;
+  private int mailboxCapacity;
+  private int passwordLength = 10;
+  private String alternateEmail;
+
+  // Constructor receive first and last name
+  public Email(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    System.out.println("Email created: " + this.firstName + " " + this.lastName);
+    // method for getting the department
+    this.department = setDepartment();
+    System.out.println("Department: " + this.department);
+    // method for getting random password
+    this.password = randPassword(passwordLength);
+    System.out.println("Password: " + this.password);
+  }
+  // Ask for department
+  private String setDepartment() {
+    System.out.println("Department Code\n1 for Sales\n2 for Development\n3 Accounting \n0 for none\nEnter department code: ");
+    Scanner in = new Scanner(System.in);
+    int deptChoice = in.nextInt();
+    in.close();  
+    if (deptChoice == 1) {
+      return "sales";
+    }
+    else if (deptChoice == 2) {
+      return "Development";
+    }
+    else if (deptChoice ==3) {
+      return "Accounting";
+    }
+    else {
+      return "";
+    }
+  }
+  // Generate a random password
+  private String randPassword(int length) {
+    String passwordSet = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+    char[] password = new char[length];
+    for (int i = 0; i<length; i++) {
+      int rand = (int) (Math.random() * passwordSet.length());
+      password[i] = passwordSet.charAt(rand);
+    }
+    return new String(password);
+  }
+  // Set mailbox capacity
+  
+  // Set alternate email
+  
+  // Change password
+}
